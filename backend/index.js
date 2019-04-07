5bff80d7386bc16796a4c7ebbe400a89eb658e98
Matej Sladek <matejsladek10@gmail.com>
2019-04-07 23:32:26 +0200

    add disconnect

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 const io = require('socket.io')(80);
+const cookie = require('cookie');
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,138 +13,154 @@ app.use(bodyParser.json());
 
 // set a cookie
 app.use(function (req, res, next) {
-    // check if client sent cookie
-    const cookie = req.cookies.id;
-    if (cookie === undefined) {
-        // no: set a new cookie
-        let randomNumber = Math.random().toString();
-        randomNumber = randomNumber.substring(2, randomNumber.length);
-        res.cookie('id', randomNumber);
-        console.log('cookie created successfully');
-    } else {
-        console.log('cookie exists', cookie);
-    }
-    next(); // <-- important!
+  // check if client sent cookie
+  const cookie = req.cookies.id;
+  if (cookie === undefined) {
+    // no: set a new cookie
+    let randomNumber = Math.random().toString();
+    randomNumber = randomNumber.substring(2, randomNumber.length);
+    res.cookie('id', randomNumber);
+    console.log('cookie created successfully');
+  } else {
+    console.log('cookie exists', cookie);
+  }
+  next(); // <-- important!
 });
 
 const allowedOrigins = ['http://localhost:3000',
-    'http://localhost:8080',
-    'http://yourapp.com'];
+  'http://localhost:8080',
+  'http://yourapp.com'];
 app.use(cors({
-    credentials: true,
-    origin: function (origin, callback) {
-        // allow requests with no origin
-        // (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
+  credentials: true,
+  origin: function (origin, callback) {
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
     }
+    return callback(null, true);
+  }
 }));
 
-io.on('connection', function (socket) {
-    console.log('a user connected');
-    socket.on("movePlayer", function (data) {
-        io.emit('movePlayer', data);
-        for (let i = 0; i < rooms.length; i++) {
-            const room = rooms[i];
-            if(room.id === data.roomId){
-                for (let j = 0; j < room.players.length; j++) {
-                    const player = room.players[j];
-                    if(player.id === data.player.playerId){
-                        player.x = data.player.x;
-                        player.y = data.player.y;
-                    }
-                }
-            }
+function movePlayer(data) {
+  io.emit('movePlayer', data);
+  for (let i = 0; i < rooms.length; i++) {
+    const room = rooms[i];
+    if (room.id === data.roomId) {
+      for (let j = 0; j < room.players.length; j++) {
+        const player = room.players[j];
+        if (player.id === data.player.playerId) {
+          player.x = data.player.x;
+          player.y = data.player.y;
         }
-    });
+      }
+    }
+  }
+}
 
+function disconnect(socket) {
+  console.log('disconnect');
+  const cookiesStr = socket.request.headers.cookie;
+  const cookies = cookie.parse(cookiesStr);
+  const playerId = parseInt(cookies.id);
+  for (let i = 0; i < rooms.length; i++) {
+      const room = rooms[i];
+      removePlayer(room.id, playerId);
+  }
+}
+
+io.on('connection', function (socket) {
+  socket.on("movePlayer", movePlayer);
+  socket.on('disconnect', () => disconnect(socket));
 });
 
 const rooms = [{
-    id: 1,
+  id: 1,
+  state: 'pending',
+  size: 4,
+  players: [],
+},
+  {
+    id: 2,
     state: 'pending',
     size: 4,
     players: [],
-},
-    {
-        id: 2,
-        state: 'pending',
-        size: 4,
-        players: [],
-    }];
+  }];
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/rooms', (req, res) => {
-    res.json(rooms);
+  res.json(rooms);
 });
 
 app.get('/joinRoom/:roomId', (req, res) => {
-    const roomId = parseInt(req.params.roomId);
-    const playerId = parseInt(req.cookies.id);
-    for (let i = 0; i < rooms.length; i++) {
-        const id = rooms[i].id;
-        if (id === roomId) {
-            const exists = rooms[i].players.filter(player => player.id === playerId);
-            if(exists.length > 0) continue;
-            rooms[i].players.push({
-               id: playerId,
-               alive: true,
-                pos: {
-                   x: 0,
-                   y: 0,
-                }
-            });
+  const roomId = parseInt(req.params.roomId);
+  const playerId = parseInt(req.cookies.id);
+  for (let i = 0; i < rooms.length; i++) {
+    const id = rooms[i].id;
+    if (id === roomId) {
+      const exists = rooms[i].players.filter(player => player.id === playerId);
+      if (exists.length > 0) continue;
+      rooms[i].players.push({
+        id: playerId,
+        alive: true,
+        pos: {
+          x: 0,
+          y: 0,
         }
+      });
     }
-    res.json(rooms);
+  }
+  res.json(rooms);
 });
 
-app.get('/leaveRoom/:roomId', (req, res) => {
-    const roomId = parseInt(req.params.roomId);
-    const playerId = parseInt(req.cookies.id);
-    for (let i = 0; i < rooms.length; i++) {
-        const id = rooms[i].id;
-        if (id === roomId) {
-            const players = [];
-            for (let j = 0; j < rooms[i].players.length; j++){
-                if(rooms[i].players[j].id !== playerId){
-                    players.push(rooms[i].players[j]);
-                }
-            }
-            rooms[i].players = players;
+function removePlayer(roomId, playerId) {
+  for (let i = 0; i < rooms.length; i++) {
+    const id = rooms[i].id;
+    if (id === roomId) {
+      const players = [];
+      for (let j = 0; j < rooms[i].players.length; j++) {
+        if (rooms[i].players[j].id !== playerId) {
+          players.push(rooms[i].players[j]);
         }
+      }
+      rooms[i].players = players;
     }
-    res.json(rooms);
+  }
+}
+
+app.get('/leaveRoom/:roomId', (req, res) => {
+  const roomId = parseInt(req.params.roomId);
+  const playerId = parseInt(req.cookies.id);
+  removePlayer(roomId, playerId);
+  res.json(rooms);
 });
 
 app.get('/startGame/:roomId', (req, res) => {
-    const roomId = req.params.roomId;
-    for (let i = 0; i < rooms.length; i++) {
-        const id = rooms[i].id;
-        if (id === roomId) {
-            rooms[i].state = 'started';
-        }
+  const roomId = req.params.roomId;
+  for (let i = 0; i < rooms.length; i++) {
+    const id = rooms[i].id;
+    if (id === roomId) {
+      rooms[i].state = 'started';
     }
-    io.emit('startGame', {roomId});
-    res.sendStatus(200);
+  }
+  io.emit('startGame', {roomId});
+  res.sendStatus(200);
 });
 
 app.get('/movePlayer', (req, res) => {
-    const roomId = req.body;
-    for (let i = 0; i < rooms.length; i++) {
-        const id = rooms[i].id;
-        if (id === roomId) {
-            rooms[i].state = 'started';
-        }
+  const roomId = req.body;
+  for (let i = 0; i < rooms.length; i++) {
+    const id = rooms[i].id;
+    if (id === roomId) {
+      rooms[i].state = 'started';
     }
-    io.emit('startGame', {roomId});
-    res.sendStatus(200);
+  }
+  io.emit('startGame', {roomId});
+  res.sendStatus(200);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
