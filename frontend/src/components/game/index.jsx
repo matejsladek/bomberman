1b4d48f951eeadd2cf69fb0fe7f9992eaf218791
Matej Sladek <matejsladek10@gmail.com>
2019-04-07 16:56:40 +0200

    add phaser

--- frontend/src/components/game/index.jsx
+++ frontend/src/components/game/index.jsx
@@ -1,42 +1,104 @@
 import React from 'react';
-import * as PIXI from 'pixi.js';
+import * as Phaser from 'phaser';
+import {getCookie} from '../helpers';
+
 // import style from './lobby.css';
 
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.setup = this.setup.bind(this);
+    this.preload = this.preload.bind(this);
+    this.create = this.create.bind(this);
+    this.update = this.update.bind(this);
   }
 
   componentDidMount() {
-    this.app = new PIXI.Application(600, 600);
-    this.gameCanvas.appendChild(this.app.view);
-    this.app.start();
-    this.setup();
+    this.config = {
+      type: Phaser.AUTO,
+      width: 600,
+      height: 600,
+      scene: {
+        preload: this.preload,
+        create: this.create,
+        update: this.update,
+      },
+      physics: {
+        default: 'arcade',
+      },
+      parent: "parent-game",
+    };
+
+    this.game = new Phaser.Game(this.config);
   }
 
-  setup(){
-      const graphics = new PIXI.Graphics();
-      graphics.beginFill(0xFFFF00);
-      graphics.drawRect(0, 0, 100, 100);
-      this.app.stage.addChild(graphics);
+  preload() {
+    const scene = this.game.scene.keys["default"];
+    const starPath = require("../../../assets/star.png");
+    scene.load.image('star', starPath);
+    console.log('this preload', this);
   }
 
-  componentWillUnmount() {
-    this.app.stop();
+  create() {
+    const scene = this.game.scene.keys["default"];
+    this.cursors = scene.input.keyboard.createCursorKeys();
+    this.players = [];
+    for (let i = 0; i < this.props.room.players.length; i++) {
+      const playerProps = this.props.room.players[i];
+      const player = scene.physics.add.sprite(100, 450, 'star');
+      player.setBounce(0.2);
+      player.setCollideWorldBounds(true);
+      player.playerId = playerProps.id;
+      this.players.push(player);
+    }
+  }
+
+  getPlayer(playerId) {
+    for (let i = 0; i < room.players.length; i++) {
+      if (room.players[i].id === playerId) return room.players[i];
+    }
+  }
+
+  // setPlayer(playerId){
+  // for (let i = 0; i < room.players.length; i++) {
+  //         if(room.players[i].id === playerId) return room.players[i];
+  //     }
+  // }
+  update() {
+    const id = parseInt(getCookie('id'));
+    let player = null;
+    for (let i = 0; i < this.players.length; i++) {
+      const x = this.players[i];
+      if(x.playerId === id) player = x;
+    }
+    if(this.cursors.left.isDown) {
+      player.setVelocityX(-160);
+    } else if (this.cursors.right.isDown) {
+      player.setVelocityX(160);
+    } else {
+      player.setVelocityX(0);
+    }
+    if(this.cursors.up.isDown) {
+      player.setVelocityY(-160);
+    } else if (this.cursors.down.isDown) {
+      player.setVelocityY(160);
+    } else {
+      player.setVelocityY(0);
+    }
   }
 
   render() {
-      let component = this;
-      console.log('this state', this.props.room);
+    let component = this;
+    console.log('this state', this.props.room);
     return (
-        <div>
-          <button type="button" className="btn btn-secondary" onClick={this.props.backToLobby}>
-            Back
-          </button>
-          game:
-          <div ref={(thisDiv) => {component.gameCanvas = thisDiv}} />
-        </div>
+      <div>
+        <button type="button" className="btn btn-secondary" onClick={this.props.backToLobby}>
+          Back
+        </button>
+        game:
+        <div id="parent-game" ref={(thisDiv) => {
+          component.gameCanvas = thisDiv
+        }}/>
+      </div>
     );
   }
 }
